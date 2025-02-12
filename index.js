8ba7c4f9807310e80921cc0fb72943469079bc8e
IvanRecio <irecio@m-shop.mobi>
2025-02-12 13:10:19 +0100

    Ejercicio Terminado

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use('/tasks', taskRoutes);
 const swaggerSpecs = swaggerJsdoc({ swaggerDefinition: swaggerDocument, apis: ['./docs/*.js'] });
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
-dbConnection();
+dbConnection;
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
